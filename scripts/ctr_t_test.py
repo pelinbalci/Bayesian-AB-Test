9fb5e862ba62bf2b682ecb55431bb3fc5f9ef5de
Pelin Balci <balci.pelin@gmail.com>
2020-04-18 14:56:39 +0300

    fix the path

--- scripts/ctr_t_test.py
+++ scripts/ctr_t_test.py
@@ -1,15 +1,13 @@
+import os
 import pandas as pd
 import numpy as np
 from scipy import stats
+from statsmodels.stats.proportion import proportions_ztest
 
-df = pd.read_csv("/Users/pelin.balci/PycharmProjects/Bayesian-AB-Test/scripts/advertisement_clicks.csv", sep = ",")
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'advertisement_clicks.csv')
+df = pd.read_csv(path)
 control = df[df.advertisement_id == 'A']
 test = df[df.advertisement_id == 'B']
-
-print(control.head())
-
-print('number of observations in control:', len(control))
-print('number of observations in test:', len(test))
 
 control_obs = len(control)
 test_obs = len(test)
@@ -23,6 +21,20 @@ test_mean = test.action.mean()
 control_std = control.action.std()
 test_std = test.action.std()
 
+# Standard t test
+t1, p1 = stats.ttest_ind(control.action, test.action)
+
+# Welch's t test
+t2, p2 = stats.ttest_ind(control.action, test.action, equal_var=False)
+
+# Proportions z test
+count = np.array([control_sum, test_sum])
+nobs = np.array([control_obs, test_obs])
+stat, pval = proportions_ztest(count, nobs)
+
+
+print('number of observations in control:', len(control))
+print('number of observations in test:', len(test))
 
 print('sum of control:', control.action.sum())
 print('sum of test:', test.action.sum())
@@ -33,19 +45,9 @@ print('mean of test:', test.action.mean())
 print('std of control:', control.action.std())
 print('std of test:', test.action.std())
 
-t1, p1 = stats.ttest_ind(control.action, test.action)
-print("standart ttest", "t1 = {:.10f}".format(t1), "p1 = {:.6f}".format(p1))
-
-
-t2, p2 = stats.ttest_ind(control.action, test.action, equal_var=False)
-print("welch's ttest", "t2 = {:.10f}".format(t2), "p2 = {:.6f}".format(p2))
-
-import numpy as np
-from statsmodels.stats.proportion import proportions_ztest
-count = np.array([control_sum, test_sum])
-nobs = np.array([control_obs, test_obs])
-stat, pval = proportions_ztest(count, nobs)
-print('{0:0.6f}'.format(pval))
+print("standart ttest", "t = {:.10f}".format(t1), "p = {:.6f}".format(p1))
+print("welch's ttest", "t = {:.10f}".format(t2), "p = {:.6f}".format(p2))
+print('proportions z test p = {0:0.6f}'.format(pval))
 
 
 
