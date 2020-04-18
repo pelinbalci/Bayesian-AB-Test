29a5e6447b0060d864ba672e1ffd5969313f8d8b
Pelin Balci <balci.pelin@gmail.com>
2020-04-18 14:49:29 +0300

    fix the path

--- scripts/chi_square_test.py
+++ scripts/chi_square_test.py
@@ -1,15 +1,12 @@
+import os
 import pandas as pd
 import numpy as np
-from scipy.stats import chi2, chi2_contingency
+from scipy.stats import chi2
 
-df = pd.read_csv("/Users/pelin.balci/PycharmProjects/Bayesian-AB-Test/scripts/advertisement_clicks.csv", sep = ",")
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'advertisement_clicks.csv')
+df = pd.read_csv(path)
 control = df[df.advertisement_id == 'A']
-test =  df[df.advertisement_id == 'B']
-
-print(control.head())
-
-print('number of observations in control:', len(control))
-print('number of observations in test:', len(test))
+test = df[df.advertisement_id == 'B']
 
 control_obs = len(control)
 test_obs = len(test)
@@ -23,6 +20,20 @@ test_mean = test.action.mean()
 control_std = control.action.std()
 test_std = test.action.std()
 
+T = np.zeros((2, 2)).astype(np.float32)
+T[0, 0] = control_sum
+T[0, 1] = control_obs - control_sum
+
+T[1, 0] = test_sum
+T[1, 1] = test_obs - test_sum
+
+det = T[0, 0]*T[1, 1] - T[0, 1]*T[1, 0]
+c2 = float(det) / T[0].sum() * det / T[1].sum() * T.sum() / T[:,0].sum() / T[:,1].sum()
+p = 1 - chi2.cdf(x=c2, df=1)
+
+
+print('number of observations in control:', len(control))
+print('number of observations in test:', len(test))
 
 print('sum of control:', control.action.sum())
 print('sum of test:', test.action.sum())
@@ -32,20 +43,4 @@ print('mean of test:', test.action.mean())
 
 print('std of control:', control.action.std())
 print('std of test:', test.action.std())
-
-T = np.zeros((2, 2)).astype(np.float32)
-T[0,0] = control_sum
-T[0,1] = control_obs - control_sum
-
-T[1,0] = test_sum
-T[1,1] = test_obs - test_sum
-
-#c2 = chi2_contingency(T, correction=False)
-#p = 1 - chi2.cdf(x=c2, df=1)
-
-det = T[0,0]*T[1,1] - T[0,1]*T[1,0]
-c2 = float(det) / T[0].sum() * det / T[1].sum() * T.sum() / T[:,0].sum() / T[:,1].sum()
-p = 1 - chi2.cdf(x=c2, df=1)
-
-
 print("p = {:.6f}".format(p))
